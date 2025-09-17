70de4446b31a15fb26f0f056cea3a9dad9187e31
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-09-17 18:15:31 +0300

    benefits of react

--- index.jsx
+++ index.jsx
@@ -1,9 +1,18 @@
 // import{ cresteRoot }  from "react-dom/client"       
-   import ReactDom from "react-dom/client"      
+   //import ReactDom from "react-dom/client"      
          
          // 1. create a root
          //const root = createRoot(document.getElementById("root"))
          // // 2. render some markup to the root
          //root.render(<h1>Hello, React!</h1>)
          
-         ReactDom.createRoot(document.querySelector("#root")).render(<p>hello react world</p>)
+        // ReactDom.createRoot(document.querySelector("#root")).render(<p>hello react world</p>)
+
+        import {createRoot} from "react-dom/client"
+        createRoot(document.getElementById("root")).render(
+            <ul>
+                <Li>super popular js library</Li>
+                <Li>will help me be even more employable</Li>
+                <Li>React has a pretty cool logo</Li>
+            </ul>
+        )
