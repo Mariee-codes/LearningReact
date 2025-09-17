1a3bba6b76bdcd57fe9c1ca23f478246f7cec63e
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-09-17 14:23:11 +0300

    creating roots

---
+++ index.jsx
@@ -0,0 +1,7 @@
+ import{ cresteRoot }  from "react-dom/client"       
+         
+         
+         // 1. create a root
+         const root = createRoot(document.getElementById("root"))
+         // // 2. render some markup to the root
+         root.render(<h1>Hello, React!</h1
