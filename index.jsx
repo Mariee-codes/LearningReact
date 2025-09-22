04a859314d1c796d84c892bb0e311f209150bde0
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-09-22 13:31:01 +0300

    creating elements

--- index.jsx
+++ index.jsx
@@ -8,12 +8,21 @@
          
         // ReactDom.createRoot(document.querySelector("#root")).render(<p>hello react world</p>)
 
-        import {createRoot} from "react-dom/client"
-        createRoot(document.getElementById("root")).render(
-            <ul>
-                <Li>super popular js library</Li>
-                <Li>will help me be even more employable</Li>
-                <Li>React has a pretty cool logo</Li>
-                <li>i love react</li>
-            </ul>
+        // import {createRoot} from "react-dom/client"
+        // createRoot(document.getElementById("root")).render(
+        //     <ul>
+        //         <Li>super popular js library</Li>
+        //         <Li>will help me be even more employable</Li>
+        //         <Li>React has a pretty cool logo</Li>
+        //         <li>i love react</li>
+        //     </ul>
+        // )
+
+
+        import { createElement } from "react"
+        import { createElement } from "react-dom/client"
+
+        const root = createRoot(document.getElementById("root"))
+        root.render(
+            createElement("h1", null, "Hello from createElement")
         )
