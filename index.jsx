06e3d5f4fa2ae62db4abeef56561efd375589b6d
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-09-24 16:28:48 +0300

    creating new react app

--- index.jsx
+++ index.jsx
@@ -36,13 +36,37 @@ create your very first custom react component
 call it "main content", and have it return a simple h1 element that says "react is great"
 afterward, render it on the line below MyAwesomeNavbar
 */
-function MainContent() {
-    return <h1>React is great!</h1>
-}
 
-root.render (
-<div>
-    <MyAwesomeNavbar />
-    <MainContent />
-</div>
-)
+// function MainContent() {
+//     return <h1>React is great!</h1>
+// }
+
+// root.render (
+// <div>
+//     <MyAwesomeNavbar />
+//     <MainContent />
+// </div>
+// )
+
+/*
+create a new h1 element (createElement)
+give it some textContent
+give it a class of "header"
+append it as a child(using 'appendChild') of the div#root
+dont use inner html
+*/
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is imperative coding"
+// h1.className = "header"
+// document.getElementById("root").appendChild(h1)
+
+/*
+set up a new react app from scratch.
+hints: need to import the method to create a new root from the "react-dom/client package)
+"
+use that root to render some jsx .for now, just render a h1 element that says anythung you want.
+*/
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+root.render(<h1>This is React</h1>)
