6ada5f7ae62e9627dff4fa9d93554cd6d8f98927
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-10-16 10:47:32 +0300

    react

--- index.jsx
+++ index.jsx
@@ -1,8 +1,9 @@
-// import{ cresteRoot }  from "react-dom/client"       
+import{ cresteRoot }  from "react-dom/client" 
+import      
    //import ReactDom from "react-dom/client"      
          
          // 1. create a root
-         //const root = createRoot(document.getElementById("root"))
+         const root = createRoot(document.getElementById("root"))
          // // 2. render some markup to the root
          //root.render(<h1>Hello, React!</h1>)
          
