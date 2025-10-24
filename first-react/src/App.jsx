e976e579044b7f385ff458dbc7a14a6df0917d02
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-10-24 16:40:00 +0300

    learning react

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -2,32 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* header */}
+  <Header/>
+    {/* body */}
+
+    <div className="main--el">
+    <h2>i love my school</h2>
+    <p>
+    The default value of the position property in CSS is static. 
+This means that elements are positioned according to the normal flow of the document. When an element has position: static;, the top, right, bottom, left, and z-index properties have no effect on its placement. 
+    </p>
+    </div>
+    
+
+    {/* footer */}
+  <footer className='footer--el'>
+    <p>copyright 2025</p>
+  </footer>
     </>
   )
 }
---
+++ first-react/src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Header (){
+
+    return(
+        <>
+
+        <div className='Appbar'>
+        <h1>Dribble</h1>
+        <h2>home</h2>
+        <h2>about us</h2>
+      </div>
+      </>
+
+    )
+
+}
+export default Header;
