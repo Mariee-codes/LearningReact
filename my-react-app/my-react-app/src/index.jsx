31e6b8ee3941b60d06ab4996669908a465920cd6
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-10 15:34:26 +0300

    props

--- my-react-app/my-react-app/src/index.jsx
+++ my-react-app/my-react-app/src/index.jsx
@@ -147,21 +147,21 @@ const root = createRoot(document.getElementById("root"))
 root.render(<App />)
 
 
-import ReactDOM  from 'react-dom/client';
-function APP(){
-    const hours = new Date().getHours()
-    let timeOfDay
+// import ReactDOM  from 'react-dom/client';
+// function APP(){
+//     const hours = new Date().getHours()
+//     let timeOfDay
 
-    if(hours<12){
-        timeOfDay="morning"
-    }
-    else if (hours>= 12 && hours <17){
-        timeOfDay="afternoon"
-    }
-    else{
-        timeOfDay="evening"
-    }
-    return(
-        <h1>goodnight</h1>
-    )
-}
+//     if(hours<12){
+//         timeOfDay="morning"
+//     }
+//     else if (hours>= 12 && hours <17){
+//         timeOfDay="afternoon"
+//     }
+//     else{
+//         timeOfDay="evening"
+//     }
+//     return(
+//         <h1>goodnight</h1>
+//     )
+// }
