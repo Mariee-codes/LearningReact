fb1c0cd5f06c2e612bba8f17e5612042d827ee98
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-10 15:53:39 +0300

    props practice

---
+++ my-react-app/app.jsx
@@ -0,0 +1,22 @@
+/**
+ * challenge: create a page that displays your favourite jokes
+ * create a joke component on its own file
+ * import and render 4-5 <joke /> components
+ * each joke should receive a "setup" prop and "punchline" prop 
+ * and render those you'd like
+ * use your favourite 2-parts jokes (setup & punchline), or check
+ * jokes.md file for some examples.
+ * 
+ * EXTRA CREDIT
+ * some jokes are only a punchline with no set up
+ * e.g "its hard to explain puns to kleptomaniacs because
+ * they always takes things literally"
+ * if you dont pass in a question prop ,how might you make only show the 
+ * punchline.
+ */
+
+export default function App() {
+    return(
+        <></>
+    )
+}
