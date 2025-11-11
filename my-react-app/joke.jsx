d048ba449cda641bd0f8b7cba6d66248828e3430
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-11 11:27:10 +0300

    props in react

--- my-react-app/joke.jsx
+++ my-react-app/joke.jsx
@@ -1,9 +1,7 @@
 export default function Joke(props) {
     return (
         <>
-        <p style= {{display: props.setup? "block" : "none"}} className = "setup">Setup:
-            {props.setup}
-        </p>
+        {props.setup && <p className="setup">Setup: {props.setup}</p>}
         <p className="punchline">punchline: {props.punchline}</p>
         <hr />
         </>
