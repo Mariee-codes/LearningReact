939540851f5b220257a81652424f345a8267eab7
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-11 16:23:55 +0300

    implementing props

--- my-react-app/my-react-app/src/App.jsx
+++ my-react-app/my-react-app/src/App.jsx
@@ -20,8 +20,9 @@ import Entry from "./components/Entry"
                            EgZjaHJvbWUyBggAEEUYOTIJCAEQIRgKGKABMgcIAhAhGI8CMgcIAxA
                             hGI8C0gEJMTk3MThqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
            dates= " 12 Jan, 2021- 24 Jan, "
-           text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 metres (12,380<br></br>
-                  feet). <br></br>Mount Fuji is the single most popular tourist site in Japan, for both  
+           text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 metres (12,380
+                  feet). 
+                  Mount Fuji is the single most popular tourist site in Japan, for both  
                   Japanese and foreign tourists. "              
       />
       </main>
--- my-react-app/my-react-app/src/components/Entry.jsx
+++ my-react-app/my-react-app/src/components/Entry.jsx
@@ -13,18 +13,14 @@ export default function Entry(props){
     
     <div className="container-two">
         <img  className="marker"src ="../marker png.png" alt="marker icon"></img>
-         <span>JAPAN</span>
-        <a href ="https://www.google.com/search?q=mt+fuji+in+jaapn+google+maps+link&oq=mt+fuji+in+jaapn+google+maps+link&gs_lcrp=
-         EgZjaHJvbWUyBggAEEUYOTIJCAEQIRgKGKABMgcIAhAhGI8CMgcIAxA
-         hGI8C0gEJMTk3MThqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8">View on google maps</a>
+         <span className="country">{props.country}</span>
+        <a href ={props.googleMapsLink}>View on google maps</a>
 
-         <h2 className="fuji-entry">Mount Fuji</h2>
+         <h2 className="fuji-entry">{props.title}</h2>
 
-         <p className="p-entry">12 Jan, 2021- 24 Jan,
-             <br></br>2021</p>
-        <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 metres (12,380<br></br>
-         feet). <br></br>Mount Fuji is the single most popular tourist site in Japan, for both  
-        Japanese and foreign tourists. </p>
+         <p className="p-entry">{props.dates}
+             </p>
+        <p>{props.text} </p>
                                       
 
      </div>
