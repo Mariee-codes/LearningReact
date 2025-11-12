f1103a1b70e041a628381ca44dea1725a4d22aae
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-12 23:39:33 +0300

    props examples with arrays

--- my-react-app/index.jsx
+++ my-react-app/index.jsx
@@ -5,3 +5,11 @@ const nums =[1,2,3,4,5]
 //-->       [1,4,9,16,25]
 // your code here
 nums.map(function)
+
+/*
+chalenge 2:
+given an array of strings ,return an array where the first letter of each string is capitalized
+*/
+
+const name =["alice","bob","charlie","danielle"]
+// -->      []
