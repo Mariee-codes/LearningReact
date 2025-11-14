dc4302ba8f9bbd61ae794ecbf01d3359ce180ae9
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-14 15:12:28 +0300

    props and arrays

--- my-react-app/index.jsx
+++ my-react-app/index.jsx
@@ -4,7 +4,12 @@ given an array of numbers, return an array of each number, squared*/
 const nums =[1,2,3,4,5]
 //-->       [1,4,9,16,25]
 // your code here
-nums.map(function)
+const squares = nums.map(function(num) {
+    return num * num
+})
+
+console.log(squares)
+
 
 /*
 chalenge 2:
