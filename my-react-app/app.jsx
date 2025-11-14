d272233a2be5c774bbca86a801f6bb9d62503aa2
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-14 16:03:49 +0300

    rendering arrays into jsx elems by react

---
+++ my-react-app/app.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+    const ninjaTurtles = [
+        <h2>Donatello</h2>,
+        <h2>Michaelonglo</h2>,
+        <h2>Raphael"</h2>,
+        <h2>Leornado</h2>]
+    return(
+        <main>
+            {ninjaTurtles}
+        </main>
+    )
+}
+
+
