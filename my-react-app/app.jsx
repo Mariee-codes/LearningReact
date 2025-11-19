a4fdfd7f515d29fe176bf4863065eb3a4f22c395
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-19 18:49:18 +0300

    mapping components

--- my-react-app/app.jsx
+++ my-react-app/app.jsx
@@ -2,7 +2,10 @@ import Joke from "./joke"
 import jokesData from "./jokesData" 
 
 export default function App() {
-console.log(jokesData)
+const jokeElements = jokesData.map((joke) =>{
+    return <Joke setup = {joke.setup} punchline={joke.punchline}  />
+}
+)
 
     const ninjaTurtles = [
         <h2>Donatello</h2>,
--- my-react-app/joke.jsx
+++ my-react-app/joke.jsx
@@ -1,5 +1,13 @@
 
-
+export default function Joke(props) {
+    return (
+        <>
+        {props.setup && <p className="setup">Setup: {props.setup}</p>}
+        <p className="punchline">Punchline: {props.punchline}</p>
+        <hr />
+        </>
+    )
+}
 
 
 
