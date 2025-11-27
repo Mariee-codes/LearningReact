fc8049a3db85ecb175fe3b8d7f6c28afa037c49b
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-27 23:57:05 +0300

    new export data file

--- my-react-app/app.jsx
+++ my-react-app/app.jsx
@@ -1,26 +1,26 @@
-import Joke from "./joke"
-import jokesData from "./jokesData" 
+// import Joke from "./joke"
+// import jokesData from "./jokesData" 
 
-export default function App() {
-const jokeElements = jokesData.map((joke) =>{
-    return <Joke setup = {joke.setup} punchline={joke.punchline}  />
-}
-)
+// export default function App() {
+// const jokeElements = jokesData.map((joke) =>{
+//     return <Joke setup = {joke.setup} punchline={joke.punchline}  />
+// }
+// )
 
-    const ninjaTurtles = [
-        <h2>Donatello</h2>,
-        <h2>Michaelonglo</h2>,
-        <h2>Raphael"</h2>,
-        <h2>Leornado</h2>]
-    return(
-        <main>
-            {ninjaTurtles }
-        </main>
-    )
-}
+//     const ninjaTurtles = [
+//         <h2>Donatello</h2>,
+//         <h2>Michaelonglo</h2>,
+//         <h2>Raphael"</h2>,
+//         <h2>Leornado</h2>]
+//     return(
+//         <main>
+//             {ninjaTurtles }
+//         </main>
+//     )
+// }
 
 
-//mapping components
+// //mapping components
 
-for <let i=0; i<100;i++>
+
 
--- my-react-app/jokesData.js
+++ my-react-app/jokesData.js
@@ -1,31 +1,31 @@
-export default [
-    {
-    setup:"i got my daughter a fridge for her birthday"
-    punchline: "i cant wait to see her face light up when she opens it"
+// export default [
+//     {
+//     setup:"i got my daughter a fridge for her birthday"
+//     punchline: "i cant wait to see her face light up when she opens it"
 
-}
+// }
 
- {
-    setup:"i got my daughter a fridge for her birthday"
-    punchline: "i cant wait to see her face light up when she opens it"
+//  {
+//     setup:"i got my daughter a fridge for her birthday"
+//     punchline: "i cant wait to see her face light up when she opens it"
 
-}
- {
-    setup:"i got my daughter a fridge for her birthday"
-    punchline: "i cant wait to see her face light up when she opens it"
+// }
+//  {
+//     setup:"i got my daughter a fridge for her birthday"
+//     punchline: "i cant wait to see her face light up when she opens it"
 
-}
- {
-    setup:"i got my daughter a fridge for her birthday"
-    punchline: "i cant wait to see her face light up when she opens it"
+// }
+//  {
+// //     setup:"i got my daughter a fridge for her birthday"
+//     punchline: "i cant wait to see her face light up when she opens it"
 
-}
- {
-    setup:"i got my daughter a fridge for her birthday"
-    punchline: "i cant wait to see her face light up when she opens it"
+// }
+//  {
+//     setup:"i got my daughter a fridge for her birthday"
+//     punchline: "i cant wait to see her face light up when she opens it"
 
-    setup:"i got my daughter a fridge for her birthday"
-    punchline: "i cant wait to see her face light up when she opens it"
+//     setup:"i got my daughter a fridge for her birthday"
+//     punchline: "i cant wait to see her face light up when she opens it"
 
-}
-]
+// }
+// ]
