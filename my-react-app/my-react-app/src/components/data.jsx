1fa320dbe28b6ab1f1feb095311b819a3f96d799
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-11-28 00:16:58 +0300

    data entry

--- my-react-app/my-react-app/src/components/data.jsx
+++ my-react-app/my-react-app/src/components/data.jsx
@@ -2,8 +2,21 @@ export default [
     {
         id: 1,
         img: {
-            src:"mt fuji.jpeg"
-            alt
+            src:"mt fuji.jpeg" ,
+            alt:"Mt Fuji"
+        },
+
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink:"https://www.google.com/search?q=mt+fuji+in+jaapn+google+maps+link&oq=mt+fuji+in+jaapn+google+maps+link&gs_lcrp=",
+        dates:"12 Jan, 2021 - 24 Jan, 2021",
+        text:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 metres (12,380 feet).Mount Fuji is the single most popular tourist site in Japan, for both  Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        img: {
+            src:""
         }
     }
+
 ]
