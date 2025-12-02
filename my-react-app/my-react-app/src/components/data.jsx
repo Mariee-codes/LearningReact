583475fb269509cf8990a8dc4791a87809e828c4
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-12-02 17:28:52 +0300

    entries

--- my-react-app/my-react-app/src/components/data.jsx
+++ my-react-app/my-react-app/src/components/data.jsx
@@ -19,7 +19,24 @@ export default [
             alt:"Sidney Opera House"
         },
 
-        title:""
-    }
+        title:"Sydney Opera House",
+        country:"Australia",
+        googleMapsLink:"",
+        dates:"27 May, 2021 - 8 Jun, 2021",
+        text:" The Sydney Opera House is a multi-venue perfoming arts center in Sydney. Located on the banks of the Sydney Harbour, it is often regarded... "
+    },
 
+    {
+        id:3,
+        img: {
+            src:"",
+            alt:""
+        },
+
+        title:"Geirangerfjord",
+        country:"NORWAY",
+        googleMapsLink:"",
+        dates:" 01 oct, 2021 - 18 Nov, 2021",
+        text:"The Geiranger Fjord is a fjord in the "
+    }
 ]
