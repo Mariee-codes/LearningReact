8d8eac9d8d4fc9bbb779db27ccf67b706d727b20
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-12-02 14:30:05 +0300

    entries

--- my-react-app/my-react-app/src/App.jsx
+++ my-react-app/my-react-app/src/App.jsx
@@ -8,36 +8,22 @@ import data from "./data"
   const entryElements = data.map((entry) => {
     return (
       <Entry
-      img={{
-        src: entry.img.src,
-        alt: entry.img.alt
-      }}
+      img={entry.img}
+      title={entry.title}
+      country= {entry.counrty}
+      googleMapsLink= {entry.googleMapsLink}
+      dates ={ entry.dates}
+      text= {entry.text}
 
-      />
+       />
     )
-  }
-  )
+  })
   
   return (
     <>
       <Header />
       <main className='container'>
-      <Entry  
-           img={{
-            src:"mt fuji.jpeg" ,
-            alt:" Mt Fuji"
-           }}
-           title= "Mount Fuji"
-           country="JAPAN"
-           googleMapsLink="https://www.google.com/search?q=mt+fuji+in+jaapn+google+maps+link&oq=mt+fuji+in+jaapn+google+maps+link&gs_lcrp=
-                           EgZjaHJvbWUyBggAEEUYOTIJCAEQIRgKGKABMgcIAhAhGI8CMgcIAxA
-                            hGI8C0gEJMTk3MThqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
-           dates= " 12 Jan, 2021- 24 Jan, "
-           text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 metres (12,380
-                  feet). 
-                  Mount Fuji is the single most popular tourist site in Japan, for both  
-                  Japanese and foreign tourists. "              
-      />
+      {entryElements}
       </main>
     </>
     
