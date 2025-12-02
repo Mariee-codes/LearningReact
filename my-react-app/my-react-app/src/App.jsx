d6f585131ec9a36cc5cc3bdbda112630102f4447
Mariee-codes <marion.19745@students.kyu.ac.ke>
2025-12-03 01:35:24 +0300

    entries

--- my-react-app/my-react-app/src/App.jsx
+++ my-react-app/my-react-app/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
-import Header from "./components/header"
-import Entry from "./components/Entry"
-import data from "./data"
+import Header from "./components/header.jsx"
+import Entry from "./components/Entry.jsx"
+import data from "./data.jsx"
 
 
  export default function App() {
+  console.log("App is rendering") ;
+  console.log("Data:", data);
+
+
+
   const entryElements = data.map((entry) => {
+    console.log("Mapping entry:", entry);
+
     return (
       <Entry
       img={entry.img}
       title={entry.title}
-      country= {entry.counrty}
+      country= {entry.country}
       googleMapsLink= {entry.googleMapsLink}
       dates ={ entry.dates}
       text= {entry.text}
---
+++ my-react-app/my-react-app/src/data.jsx
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src:"mt fuji.jpeg",
+            alt:"Mt Fuji"
+        },
+        title: "Mount Fuji",
+        country: "JAPAN",
+        googleMapsLink:"https://www.google.com/search?q=mt+fuji+in+jaapn+google+maps+link&oq=mt+fuji+in+jaapn+google+maps+link&gs_lcrp=",
+        dates:"12 Jan, 2021 - 24 Jan, 2021",
+        text:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 metres (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        img: {
+            src:"Sidney Opera House.webp",
+            alt:"Sidney Opera House"
+        },
+        title:"Sydney Opera House",
+        country:"AUSTRALIA",
+        googleMapsLink:"https://lh3.googleusercontent.com/gps-cs-s/AG0ilSzkZWEIRtz6wY5XG1EIRyzMpPEjK0MXWny_YmPlLSU15znOad3VATzD9oeN4aSy5r0G1KOa_iXRIYOm0dPsBAM3PyFN7vOotk9-XWkZgCdH0onhAy8x8-o6W4gDbdlOHsUUwxQO=w408-h306-k-no",
+        dates:"27 May, 2021 - 8 Jun, 2021",
+        text:"The Sydney Opera House is a multi-venue performing arts center in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the most famous and distinctive buildings in the world."
+    },
+    {
+        id: 3,
+        img: {
+            src:"Geirangerfjord.jpg",
+            alt:"Geirangerfjord"
+        },
+        title:"Geirangerfjord",
+        country:"NORWAY",
+        googleMapsLink:"https://www.google.com/imgres?q=geiranger%20fjord%20google%20maps%20link&imgurl=https%3A%2F%2Fwww.geirangerfjord.no%2Fupload%2Fimages%2FMaps%2FPanorama_bus_tour_-_birdsview_2015.jpg&imgrefurl=http%3A%2F%2Fwww.geirangerfjord.no%2Fmap&docid=Y38bsWfhTupQxM&tbnid=zHS1fAurJqxVDM&vet=12ahUKEwi9k7C-p5-RAxVO0wIHHRTQGMAQM3oECBwQAA..i&w=1000&h=750&hcb=2&ved=2ahUKEwi9k7C-p5-RAxVO0wIHHRTQGMAQM3oECBwQAA",
+        dates:"01 Oct, 2021 - 18 Nov, 2021",
+        text:"The Geiranger Fjord is a fjord in the Sunnmøre region of Norway. It is a UNESCO World Heritage Site."
+    }
+]
